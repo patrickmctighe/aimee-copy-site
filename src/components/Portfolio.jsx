c1d9441809db88c22c39ab29b052bebda011df76
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2024-02-09 15:18:40 -0500

    router-added

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,18 @@
+
+
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>Check out the following things Ive done:</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Portfolio;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Portfolio from './components/Portfolio.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> },
+  { path: '/portfolio', element: <Portfolio /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
